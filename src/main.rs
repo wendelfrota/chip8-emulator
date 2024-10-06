0043250b008edc3f93dab132a6a9b468d0974d90
wendelfrota <wendelalves898@gmail.com>
2024-10-06 11:51:19 -0300

    update: create games directory and move it

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,33 @@
 mod cli;
 mod display;
 mod frame;
+mod opcode;
 
 use clap::{Parser};
 use crate::cli::{Commands, Cli};
 use crate::frame::Frame;
+use std::fs;
+
 
 fn main() {
     let cli = Cli::parse();
+
     match cli.command {
         Commands::Add(add_command) => {
-            println!("{} added.", add_command.game);
+            match fs::metadata(&add_command.game) {
+                Ok(metadata) => {
+                    if let Err(e) = fs::create_dir_all("./src/games") {
+                        eprintln!("Failed to create directory: {}", e);
+                        return;
+                    }
+
+                    if metadata.is_file(){
+                        fs::rename(&add_command.game, format!("./src/games/{}", add_command.game)).expect("Failed to move game file");
+                        println!("{} added.", add_command.game);
+                    }
+                }
+                Err(e) => { println!("{}", e); }
+            }
         }
         Commands::Start(start_command) => {
             println!("Memory Allocated: {} KB", start_command.memory);
